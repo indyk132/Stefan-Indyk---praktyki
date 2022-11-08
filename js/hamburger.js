4faafbef12314f95bb6f7ce5ef6c578b82ce0dbb
Stefan <indyks@gmail.com>
2022-11-08 08:00:31 +0100

    '#9 poprawione nazwy'

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,3 +2,5 @@ const hamburgerMenu = document.querySelector('.menu');
 function myFunction(){
     hamburgerMenu.classList.toggle('animation-switch')
 }
+
+hamburgerMenu.addEventListener('click', myFunction)
