dc36d8c6eb12bc4362a81b485cdb395eb128e297
Stefan <indyks@gmail.com>
2022-11-08 11:45:56 +0100

    '#7 zdjęcia w main '

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.querySelector('.menu');
+const menuBar = document.querySelector('.burger_menu')
 function myFunction(){
     hamburgerMenu.classList.toggle('animation-switch')
 }
 
-hamburgerMenu.addEventListener('click', myFunction)
+menuBar.addEventListener('click', myFunction)
